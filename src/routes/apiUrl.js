d647e03f05d2977a0d9e5283aaa7106b938618c2
Ale Grosso <grossom@gmail.com>
2024-05-28 23:53:32 -0300

    Update new apiUrl.js

--- src/routes/apiUrl.js
+++ src/routes/apiUrl.js
@@ -1,6 +1,7 @@
 // Esta url luego se debe modificar con la de la api real, esta es solo la de prueba
 
-export const API_BASE_URL = 'https://m4legrosso.pythonanywhere.com/'
+// export const API_BASE_URL = 'https://m4legrosso.pythonanywhere.com/'
+export const API_BASE_URL = 'https://grosso4le.pythonanywhere.com/'
 
 export const ENDPOINTS = {
   login: 'login',
